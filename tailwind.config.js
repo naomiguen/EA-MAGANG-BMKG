519480c08f1939dbc146d414d871678f249db74b
guen <naomiguen4@gmail.com>
2026-01-29 09:01:06 +0800

    penambahan color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:{
+          50: '#f0f7ff',  
+          100: '#bfe2ff', 
+          200: '#99d4ff', 
+          300: '#80c6ff', 
+          400: '#3e9cff',
+          500: '#0064b5', //(WARNA UTAMA / BRAND)
+          600: '#00569b', // (Hover State / Tombol Tekan)
+          700: '#00467f', // (Navbar / Footer / Teks Gelap)
+          800: '#003660', 
+          900: '#002541', 
+          950: '#001325',
+        },
+        secondary:{
+          500: '#fbbf24',
+          600: '#d97706',
+        }
+      },
+      fontFamily:{
+        sans: ['Iter', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
